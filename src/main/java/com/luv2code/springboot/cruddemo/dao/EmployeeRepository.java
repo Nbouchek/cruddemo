91203244c577864b940ee78d7b4ad7d0cf10643c
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-18 09:42:44 +0100

    Change page size

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,7 +4,7 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // There is no ned to write any code :)
 }
